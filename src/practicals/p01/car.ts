a74598f15da493218ad9e52d8fe8a9f68861be04
bouquetofroses <valenny1402@gmail.com>
2026-02-20 17:44:17 +0700

    1

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,17 @@
-export class Car {}
+export class Car {
+    brand:string = "";
+    model:string = "";
+
+    start():void {
+        console.log(this.brand + " " + this.model + " is starting...");
+    }
+    accelerate():void {
+        console.log(this.brand + " " + this.model + " is accelerating...");
+    }
+    brake():void {
+        console.log(this.brand + " " + this.model + " is breaking...");
+    }
+    stop():void {
+        console.log(this.brand + " " + this.model + " is stoping...");
+    }
+}
--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,1 +1,33 @@
-export class OnlineCourse {}
+export class OnlineCourse {
+    public courseName: string ;
+    public maxStudents: number ;
+    private enrolledStudemts: number = 0  ;
+    private isOpen : boolean ;
+
+    constructor OnlineCourse ( courseName: string , maxStudents: number){
+        this.courseName = courseName ;
+        this.maxStudents = maxStudednts ;
+    }
+
+    public enroll() : boolean {
+
+    }
+
+    public closeCourse(): void {
+
+
+    }
+
+    public getAvailableSeats(): number {
+
+    }
+
+    public getCourseStatus(): strig {
+
+    }
+
+    private canEnroll():boolean {
+        
+    }
+
+}
