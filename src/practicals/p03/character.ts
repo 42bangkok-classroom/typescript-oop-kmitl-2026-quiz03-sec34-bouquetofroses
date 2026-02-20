e0a8b0fa46154cfa4416987a27dc44e0cfb86ca5
bouquetofroses <valenny1402@gmail.com>
2026-02-20 18:24:18 +0700

    8

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,6 +1,16 @@
 export class Character {
     name:string = "";
     health:number = 100 ;
-    
+    constructor (name:string , health:number){
+        this.name = name ;
+        this.health = health ;
+    }
+    getName(){
+
+    }
+
+    getHealth(){
+
+    }
 
 }
