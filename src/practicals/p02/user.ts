2dbcd64c10a1888186abf205c4d26590e8f8578b
bouquetofroses <valenny1402@gmail.com>
2026-02-20 17:52:43 +0700

    2

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,19 @@
-export class User {}
+export class User {
+    firstname:string = "";
+    lastname:string = "";
+    private age: number = 0;
+
+    setFirstname(firstname:string){
+        th
+    }
+    setAge(age: number): void {
+        this.age = age;
+     }
+    getAge(): number {
+        return this.age;
+    }
+    getFullName(): string {
+        return this.firstname + " " + this.lastname;
+    }
+}
+
